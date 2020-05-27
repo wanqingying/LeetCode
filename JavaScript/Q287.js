7892c867d0b10f145f5fb5d0d778ff0b237635cc
wanqingying <wanqingying@bytedance.com>
2020-05-27 15:01:38 +0800

    q287

--- JavaScript/Q287.js
+++ JavaScript/Q287.js
@@ -11,17 +11,16 @@
  输出: 3
 
  说明：
-
  不能更改原数组（假设数组是只读的）。
  只能使用额外的 O(1) 的空间。
  时间复杂度小于 O(n2) 。
  数组中只有一个重复的数字，但它可能不止重复出现一次。
-
  链接：https://leetcode-cn.com/problems/find-the-duplicate-number
+
  */
 
 /**
- *
+ * 暴力破解法，时间O(n^2),空间O(1)
  * @param {number[]} nums
  * @return {number}
  */
@@ -42,6 +41,7 @@ var findDuplicate = function (nums) {
 
 /**
  *
+ * 二分查找，时间O(n * log n),空间O(1)
  * @param {number[]} nums
  * @return {number}
  */
@@ -49,31 +49,42 @@ var findDuplicate2 = function (nums) {
     let start = 0;
     let end = Math.ceil(nums.length / 2)
     let tail = nums.length
-    while (start !== end) {
+    while ((end - start) > 50) {
         let k = 0;
         for (let j = 0; j < nums.length; j++) {
-            if (nums[j]) {
-                n++
-            }
-            if (n > 1) {
-                return nums[j]
+            if (start <= nums[j] && nums[j] <= end) {
+                k++
             }
         }
-    }
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = 0; j < nums.length; j++) {
-            if (nums[j] === i) {
-                n++
-            }
-            if (n > 1) {
-                return nums[j]
-            }
+        if (k > (end - start + 1)) {
+            tail = end
+            end = start + Math.ceil((end - start) / 2)
+        } else {
+            start = end
+            end = tail
         }
-        n = 0
     }
+    // 当二分查找到常数范围后，使用扫描法
+    let ast = new Map()
+    for (let i = start; i < tail; i++) {
+        let cun = ast.get(nums[i]);
+        if (cun) {
+            ast.set(nums[i], cun + 1)
+        } else {
+            ast.set(nums[i], 1)
+        }
+    }
+    let rt = null;
+    Array.from(ast.keys()).forEach(n => {
+        if (ast.get(n) > 1) {
+            rt = n
+        }
+    })
+    return rt
 };
 
-let an = [3, 1, 3, 4, 2]
+let an = [3, 1, 4, 2, 5, 1]
+let a2 = [1, 2, 8, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-console.log(findDuplicate(an));
-console.log(findDuplicate2(an));
+console.log(findDuplicate(a2));
+console.log(findDuplicate2(a2));
