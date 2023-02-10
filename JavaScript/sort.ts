31dea2970c62ef85559bdb19bf06f22779d10841
wanqingying <wanqingying@meituan.com>
2023-02-10 15:13:55 +0800

    sort

--- JavaScript/sort.ts
+++ JavaScript/sort.ts
@@ -40,17 +40,38 @@ function sort_2(arr: number[]) {
 function sort_3(arr: number[]) {
   // 快速排序
   // 递归选择锚点-以锚点排序
-
-  function pos(left,right) {
-    //(left,right]
-
+  function swap(a: number, b: number) {
+    if (a === b) return;
+    const c = arr[a];
+    arr[a] = arr[b];
+    arr[b] = c;
   }
+
+  function organize(left, right) {
+    //[left,right]
+
+    if (left >= right) return;
+
+    let mid = left;
+    for (let i = left; i < right; i++) {
+      if (arr[i] <= arr[right]) {
+        swap(mid, i);
+        mid++;
+      }
+    }
+    swap(mid, right);
+    organize(left, mid - 1);
+    organize(mid, right);
+  }
+  organize(0, arr.length - 1);
+  return arr;
 }
 
 function test(sort: any) {
   console.log("sort");
-  console.log(sort([3, 2, 5, 2, 1, 7]));
+  console.log(sort([3, 2, 3, 33, 3, 3, 1]));
+  // console.log(sort([3, 2, 5, 2, 1, 7]));
 }
-test(sort_2);
+test(sort_3);
 //
 // console.log([1, 3].slice(1));
