cade5220a6f97eaaa55cbc680e5f80f968f5176e
wanqingying <wanqingying@kuaishou.com>
2023-06-15 16:07:17 +0800

    q233

---
+++ leetcode/editor/cn/Q234_回文链表.ts
@@ -0,0 +1,63 @@
+//给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：head = [1,2,2,1]
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：head = [1,2]
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点数目在范围[1, 10⁵] 内 
+// 0 <= Node.val <= 9 
+// 
+//
+// 
+//
+// 进阶：你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？ 
+//
+// Related Topics 栈 递归 链表 双指针 👍 1710 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function isPalindrome(head: ListNode | null): boolean {
+    const arr: number[] = [];
+    while (head) {
+        arr.push(head.val);
+        head = head.next;
+    }
+    let left = 0, right = arr.length - 1;
+    while (left < right) {
+        if (arr[left] !== arr[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
+
+};
+//leetcode submit region end(Prohibit modification and deletion)
